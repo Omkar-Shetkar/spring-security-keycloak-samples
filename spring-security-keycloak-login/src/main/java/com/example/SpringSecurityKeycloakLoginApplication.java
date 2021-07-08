3c5612282d565b34c58a536cc2f11f5d4cc6c426
Omkar Shetkar <omkar.shetkar@outlook.com>
2021-07-08 18:20:09 +0530

    spring security login with Keycloak

---
+++ spring-security-keycloak-login/src/main/java/com/example/SpringSecurityKeycloakLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityKeycloakLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityKeycloakLoginApplication.class, args);
+	}
+
+}
